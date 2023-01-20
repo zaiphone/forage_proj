3cc9509cac9c2eaa561126ca3b48616b25fbbac7
Zaiph <Zaiphone0108@gmail.com>
2023-01-20 11:31:48 +0000

    git

--- car.py
+++ car.py
@@ -1,15 +1,14 @@
 from datetime import datetime
-
 class Car():
     def __init__(self, engine_type, battery):
         self.engine_type = engine_type
         self.battery = battery
-
+    def years_past(original_date, years_to_add):
+        dateholder = datetime.replace(original_date,year=datetime.year(original_date) + years_to_add)
+        return dateholder
     
     def needs_service(self,total_miles,miles_last_service,warning_ind,last_service_date):
-        self.last_service_date = last_service_date
         result=False
-        self.warning_ind = warning_ind
         self.new_miles=total_miles-miles_last_service
         match self.engine_type:
             case "capulet":
@@ -19,19 +18,21 @@ class Car():
                 if self.new_miles>60000:
                     result = True
             case "sternman":
-                if self.warning_ind==True:
+                if warning_ind==True:
                     result = True
         if result != True:
+            self.last_service_date=last_service_date
             match self.battery:
                 case "Spindler":
                     if datetime.year(datetime.today()-datetime.strptime(self.last_service_date,'%Y/%m/%d'))>2:
                         result=True
                 case "Nubbin":
-                    if datetime.year(datetime.today()-datetime.strptime(self.last_service_date,'%Y/%m/%d'))>4:
-                        print('Hello')
+                    service_date = years_past   (self.last_service_date, 4)
+                    if date_which_battery_should_be_serviced_by < self.current_date:
+                        return True
         return result
 mycar=Car('sternman','Nubbin')
-mycar.needs_service(2000,1000,False,'2018/10/11')   
+mycar.needs_service(2000,1000,False,'2017/10/11')   
 
                 
                 
