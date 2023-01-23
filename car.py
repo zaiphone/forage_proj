37a0367d80b5ee3bed558f834d1be7e9803c05d7
Zaiph <Zaiphone0108@gmail.com>
2023-01-23 16:25:00 +0000

    final

--- car.py
+++ car.py
@@ -6,32 +6,35 @@ class Car():
     
     def needs_service(self,total_miles,miles_last_service,warning_ind,last_service_date):
         result=False
+        self.result=result
         self.new_miles=total_miles-miles_last_service
         self.warning_ind=warning_ind
+
         match self.engine_type:
             case "capulet":
                 if self.new_miles>30000:
-                    result = True
+                    self.result = True
             case "Willoughby":
                 if self.new_miles>60000:
-                    result = True
+                    self.result = True
             case "sternman":
                 if self.warning_ind==True:
-                    result = True
+                    self.result = True
+
         if result != True:
             self.last_service_date=last_service_date
             match self.battery:
                 case "Spindler":
                     convstr=datetime.strptime(self.last_service_date,'%Y-%m-%d')
                     if (datetime.today()-convstr).days>730:
-                        result=True
+                        self.result=True
                 case "Nubbin":
                     convstr=datetime.strptime(self.last_service_date,'%Y-%m-%d')
                     if (datetime.today()-convstr).days>1460:
-                        result=True
-        return result
-mycar=Car('sternman','Nubbin')
-mycar.needs_service(2000,1000,False,'2016-10-11')   
+                        self.result=True
+        return self.result
+
+
 
                 
                 
