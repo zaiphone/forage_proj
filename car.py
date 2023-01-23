d5164e4f40394c4086797d16041a8a7d6354140c
Zaiph <Zaiphone0108@gmail.com>
2023-01-23 15:43:10 +0000

    dateutilmod

--- car.py
+++ car.py
@@ -1,15 +1,14 @@
 from datetime import datetime
+from dateutil.parser import parse, parserinfo
 class Car():
     def __init__(self, engine_type, battery):
         self.engine_type = engine_type
         self.battery = battery
-    def years_past(original_date, years_to_add):
-        dateholder = datetime.replace(original_date,year=datetime.year(original_date) + years_to_add)
-        return dateholder
     
     def needs_service(self,total_miles,miles_last_service,warning_ind,last_service_date):
         result=False
         self.new_miles=total_miles-miles_last_service
+        self.warning_ind=warning_ind
         match self.engine_type:
             case "capulet":
                 if self.new_miles>30000:
@@ -18,17 +17,18 @@ class Car():
                 if self.new_miles>60000:
                     result = True
             case "sternman":
-                if warning_ind==True:
+                if self.warning_ind==True:
                     result = True
         if result != True:
             self.last_service_date=last_service_date
             match self.battery:
                 case "Spindler":
-                    if datetime.year(datetime.today()-datetime.strptime(self.last_service_date,'%Y/%m/%d'))>2:
+                    convstr=datetime.strptime(self.last_service_date,'%Y/%m/%d')
+                    if dateutil.relativedelta(datetime.today(),convstr).years>2:
                         result=True
                 case "Nubbin":
-                    service_date = years_past(self.last_service_date, 4)
-                    if service_date < datetime.today():
+                    convstr=datetime.strptime(self.last_service_date,'%Y/%m/%d')
+                    if dateutil.relativedelta(datetime.today(),convstr).years>4:
                         result=True
         return result
 mycar=Car('sternman','Nubbin')
