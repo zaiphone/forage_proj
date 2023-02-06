4b4368b1b16af7c90f6c241b160049b4213a7d03
Zaiph <Zaiphone0108@gmail.com>
2023-02-06 10:28:21 +0000

    final commit

--- car.py
+++ car.py
@@ -1,6 +1,8 @@
 from datetime import datetime
+import random 
 class Car():
-    def __init__(self, engine_type, battery):
+    def __init__(self, engine_type, battery, tires):
+        self.tires= tires
         self.engine_type = engine_type
         self.battery = battery
     
@@ -9,7 +11,11 @@ class Car():
         self.result=result
         self.new_miles=total_miles-miles_last_service
         self.warning_ind=warning_ind
-
+        tire_wear=[]
+        for i in range(4):
+            tire_wear.append(random.random()) #auto generate the array 0-1
+        self.tire_wear=tire_wear
+        print(self.tire_wear)
         match self.engine_type:
             case "capulet":
                 if self.new_miles>30000:
@@ -32,7 +38,23 @@ class Car():
                     convstr=datetime.strptime(self.last_service_date,'%Y-%m-%d')
                     if (datetime.today()-convstr).days>1460:
                         self.result=True
+
+        if result != True:
+            self.tire_wear=tire_wear
+            match self.tires:
+                case "Carrigan":
+                    for el in self.tire_wear:
+                        if el>=0.9:
+                            result=True
+                case "Octoprime":
+                    wear_sum=0
+                    for el in self.tire_wear:
+                        wear_sum=wear_sum+el
+                        if wear_sum>=3:
+                            result=True
+                            
         return self.result
+
 
 
 
